4c0d3d0b29e690f4a9b0d154279658e55b0876dc
ciulloa <aciullo24@gmail.com>
2018-01-02 15:23:54 +0100

    counter.R developed

--- counter.R
+++ counter.R
@@ -1,7 +1,14 @@
+###########################
+####### DESCRIPTION #######
+###########################
+
+# In this script we import the data created with "Parallel.R", we merge them
+# and we can finally select the tickers that have at least a certain number of
+# observation per day.
+
 rm (list = ls())
 suppressPackageStartupMessages(library("data.table"))
 suppressPackageStartupMessages(library("ggplot2"))
-# suppressPackageStartupMessages(library("plyr"))
 
 # Specify working directory: (The wd MUST contain 2 subfolders: "Downalod" and "Clean")
 directory = "/home/antonio/Desktop/BigData/data"
@@ -27,9 +34,16 @@ p
 # visualize the minimum number of available observation for each ticker
 min_obs <- copy(V)
 ggplot(data = unique(min_obs[, Counter:=min(Counter), by = "Ticker"])) + geom_step(aes(x = Date, y = Counter, color = as.factor(Ticker))) + theme(legend.position="none")
-rm("min_obs")
+min_obs <- min_obs[!duplicated(min_obs, by = c("Ticker","Counter")),]
+ggplot(data =  min_obs[,-1], aes(x = Ticker, y = Counter)) + geom_col() + theme(legend.position="none")
+selection <- min_obs[min_obs$Counter>=480,] # select ticker that have at least 480 observation per day, each day
+ggplot(data =  selection, aes(x = Ticker, y = Counter)) + geom_col() + theme(legend.position="none")
+#rm("min_obs")
 # visualize the average number of available observation for each ticker
 mean_obs <- copy(V)
 ggplot(data = unique(mean_obs[, Counter:=as.integer(mean(Counter)), by = "Ticker"])) + geom_step(aes(x = Date, y = Counter, color = as.factor(Ticker))) + theme(legend.position="none")
 rm("mean_obs")
+
 # total_observations <- unique(V[, Counter:=sum(Counter), by = "Ticker"])
+
+
